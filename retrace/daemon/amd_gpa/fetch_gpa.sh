505b9755a7caab8f8f27689220e0a01a7f17e417
Mark Janes <mark.a.janes@intel.com>
2018-02-22 16:50:01 -0800

    fetch script

--- retrace/daemon/amd_gpa/fetch_gpa.sh
+++ retrace/daemon/amd_gpa/fetch_gpa.sh
@@ -3,6 +3,4 @@
 git clone git://github.com/GPUOpen-Tools/GPA.git
 python -u GPA/Scripts/UpdateCommon.py
 cd GPA/Build/Linux
-sh build.sh gtestlibdir /home/majanes/src/apitrace/build/ debug skip32bitbuild skipopencl skiphsa
-
-# skiptests
+sh build.sh debug skip32bitbuild skipopencl skiphsa skiptests
